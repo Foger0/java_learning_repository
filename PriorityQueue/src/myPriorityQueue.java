778c39bc80f8ce0a14bbc1df2a4a3e1624df3dc3
lower-fall-in-the-sea <1056981071@qq.com>
2023-04-19 15:43:20 +0800

    web_learning

--- PriorityQueue/src/myPriorityQueue.java
+++ PriorityQueue/src/myPriorityQueue.java
@@ -1,4 +1,4 @@
-import java.util.PriorityQueue;
+ import java.util.PriorityQueue;
 
 public class myPriorityQueue {
     public int[] elem;
---
+++ multithreading/src/thread/ThreadDemo2.java
@@ -0,0 +1,9 @@
+package thread;
+
+
+public class ThreadDemo2 {
+    public static void main1(String[] args) {
+        
+    }
+
+}
